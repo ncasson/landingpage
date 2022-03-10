8db2735b1a778c0de6f080b8d7e0a1f25c8e5427
ncasson <ncasson9@gmail.com>
2022-03-10 01:42:39 -0600

    Add files via upload

---
+++ _layouts/todaysread.php
@@ -0,0 +1,63 @@
+<?php
+if (isset($_POST['Email'])) {
+
+    // EDIT THE FOLLOWING TWO LINES:
+    $email_to = “contact@todaysread.co“;
+    $email_subject = “New Beta Test Download”;
+
+    function problem($error)
+    {
+        echo "We're sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br><br>";
+        echo $error . "<br><br>";
+        echo "Please go back and fix these errors.<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['Email'])
+    ) {
+        problem('We're sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+    $email = $_POST['Email']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+ 
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Email: " . clean_string($email) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
+
+    Thanks for getting in touch. We'll get back to you soon.
+
+<?php
+}
+?>
