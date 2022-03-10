db29b5b90059e0571d3c7c54a4a19775acb56690
ncasson <ncasson9@gmail.com>
2022-03-10 01:46:10 -0600

    Update todaysread.php

--- _layouts/todaysread.php
+++ _layouts/todaysread.php
@@ -2,8 +2,8 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = “questtriviaapp@gmail.com“;
-    $email_subject = “New Beta Test Download”;
+    $email_to = "questtriviaapp@gmail.com";
+    $email_subject = "New form submissions";
 
     function problem($error)
     {
@@ -16,12 +16,16 @@ if (isset($_POST['Email'])) {
 
     // validation expected data exists
     if (
-        !isset($_POST['Email'])
+        !isset($_POST['Name']) ||
+        !isset($_POST['Email']) ||
+        !isset($_POST['Message'])
     ) {
         problem('We're sorry, but there appears to be a problem with the form you submitted.');
     }
 
+    $name = $_POST['Name']; // required
     $email = $_POST['Email']; // required
+    $message = $_POST['Message']; // required
 
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -32,7 +36,14 @@ if (isset($_POST['Email'])) {
 
     $string_exp = "/^[A-Za-z .'-]+$/";
 
- 
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+    }
+
     if (strlen($error_message) > 0) {
         problem($error_message);
     }
@@ -45,7 +56,9 @@ if (isset($_POST['Email'])) {
         return str_replace($bad, "", $string);
     }
 
+    $email_message .= "Name: " . clean_string($name) . "\n";
     $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
 
     // create email headers
     $headers = 'From: ' . $email . "\r\n" .
